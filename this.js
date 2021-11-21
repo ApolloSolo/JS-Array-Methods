7d017efe31601c2b28183593003e727e9f611fa0
ApolloSolo <matt1teixeira@gmail.com>
2021-11-21 12:30:22 -0500

    this

--- this.js
+++ this.js
@@ -3,6 +3,8 @@
 //but will not work the same with an arrow function =>
 //inside an arrow function, "this" is just going to refer to the scope it was created in.
 //function was created in the window Object
+//however, is we use this within a callBack like setTimeout, this was created in the scope
+//of person{}, so it will point to the person object
 const person = {
     firstName: "Matt",
     lastName: "Teixeira",
